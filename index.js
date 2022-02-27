813bf6e044a72d325dd41b0ceec65cd7398f813a
AjayGhimire <ajayghimire1998@gmail.com>
2022-02-27 12:44:28 +1100

    commit tree dba549a9bd67b91a6be9ad4d3377f60c4d9f6771

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(ride){
+ if (ride <= 400){
+   return("This one is on me!");
+ } else if (ride > 2000 && ride < 2500){
+   return("I will gladly take your thirty bucks.");
+ } else if (ride > 2500){
+   return("No can do.");
+ 
+ }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+
+
+
+function ternaryCheckCity(city){
+  // Write your code her
+if (city === "NYC"){
+  return(city ? "Ok, sounds good." : "No go.");
+} else {
+  return("No go.");
+}
 }
 
-function switchOnCharmFromTip(){
+
+const tip = "generous";
+function switchOnCharmFromTip(tip){
   // Write your code here!
+ switch(tip){
+  case "generous":
+    return ("Thank you so much.");
+    break;
+  case "not as generous":
+    return ("Thank you.");
+    break;
+  default:
+    return ("Bye.");
+
+ } 
 }
+  
+
